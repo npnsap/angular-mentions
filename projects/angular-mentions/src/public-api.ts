2ad39444757d7961ea07a543424bea7784119189
npnsap <143161837+npnsap@users.noreply.github.com>
2023-12-20 07:04:48 +0000

    Removed all except dist folder

--- projects/angular-mentions/src/public-api.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Public API Surface of angular-mentions
- */
-export * from './lib/caret-coords';
-export * from './lib/mention-utils';
-export * from './lib/mention-config';
-export * from './lib/mention-list.component';
-export * from './lib/mention.directive';
-export * from './lib/mention.module';
--- projects/demo-app/src/app/demo-async/in-memory-data.service.ts
+++
@@ -1,9 +0,0 @@
-import { InMemoryDbService } from 'angular-in-memory-web-api';
-import { COMMON_NAMES } from '../common-names';
-
-export class InMemoryDataService implements InMemoryDbService {
-  createDb() {
-    let objects = COMMON_NAMES.map(function(name){return {'label':name}});
-    return {names:COMMON_NAMES,objects:objects};
-  }
-}
